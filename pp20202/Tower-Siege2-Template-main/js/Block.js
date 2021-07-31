89593a37d0443399ad125feaffec71f20768970b
Tanuj-gamer <76273541+Tanuj-gamer@users.noreply.github.com>
2021-07-31 21:45:05 +0530

    Add files via upload

---
+++ pp20202/Tower-Siege2-Template-main/js/Block.js
@@ -0,0 +1,47 @@
+class Block{
+    constructor(x, y, width, height) {
+        var options = {
+            restitution :0.4,
+            friction :0.0,
+            isStatic : false
+        }
+        
+        this.body = Bodies.rectangle(x, y, width, height, options);
+        this.width = width;
+        this.height = height;
+         this.image= loadImage("block.png")
+        World.add(world, this.body);
+      }
+
+      display(){
+
+        var pos= this.body.position;
+        imageMode(CENTER);
+
+        if(this.body.speed <3){
+          image(this.image,pos.x,pos.y,this.width, this.height);
+        }
+        else{
+
+          //World.remove(world, this.body);
+          //World.remove(world, this.image);
+          // World.add(world, this.body);
+          // World.destroy(world, this.body);
+
+          
+          //this.visiblity = 255;
+          //this.visiblity = this.visiblity - 5;
+          //this.visiblity = this.x;
+          //this.visiblity = this.body.position.x;
+          World.remove(world,this.body);
+          push()
+          this.Visibilty = this.Visibilty-5
+          tint(255,this.Visibilty)
+      image(this.image,this.body.position.x,this.body.position.y,50,50)
+      pop()
+ 
+        }
+
+
+        }
+}
